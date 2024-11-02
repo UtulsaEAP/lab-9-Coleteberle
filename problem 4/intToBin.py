4f1f146ad5bb7be4d43bbd85ada9a937b5e76b9f
Coleteberle <cte4576@utulsa.edu>
2024-11-02 16:58:56 -0500

    problem 4

--- problem 4/intToBin.py
+++ problem 4/intToBin.py
@@ -1,16 +1,21 @@
 def int_to_reverse_binary(num1):
     binary_val = ''
 #write your while loop here
-    #while num1 > 0:
+    while num1 > 0:
         #write your code
+         binary_val += str(num1 % 2)  
+         num1 //= 2  
+
 
     return binary_val;
 
 
 def string_reverse(input_string): 
-    reverse_input = ''
+    reverse_input = input_string[::-1]
     
    #write your for loop here
+    
+
     
     return reverse_input
 
@@ -19,5 +24,5 @@ if __name__ == '__main__':
     
     binary_string = str(int_to_reverse_binary(user_input))
     binary_string = str(string_reverse(binary_string))
-    
+
     print(binary_string)
